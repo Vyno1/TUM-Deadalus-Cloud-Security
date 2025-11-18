e7da11ac85ac27d628853294b176f1815db1be12
Vyno <michael.vynogradov@tum.de>
2025-11-18 16:59:40 +0100

    added wezterm png

--- s3.tf
+++ s3.tf
@@ -23,3 +23,9 @@ resource "aws_s3_object" "fruit_salad_image" {
   key    = "fruitsalad.png"
   source = "image/fruitsalad.png"
 }
+
+resource "aws_s3_object" "wezterm_image"{
+  bucket = aws_s3_bucket.web_bucket.id
+  key = "wezterm.png"
+  source = "image/wezterm.png"
+}
